223fc1512a2c15637633b2ae2819f87724c99fe1
Kevin Bowling <kbowling@llnw.com>
2015-01-20 02:59:22 -0700

    Add FreeBSD support to ssds grain

--- salt/grains/ssds.py
+++ salt/grains/ssds.py
@@ -16,7 +16,45 @@ def ssds():
     '''
     Return list of disk devices that are SSD (non-rotational)
     '''
+    ssds = []
+
+    if grains['kernel'] == 'Linux':
+        ssds = _linux_ssds()
+    elif grains['os'] == 'FreeBSD':
+        ssds = _freebsd_ssds()
+    else:
+        log.trace('SSD grain does not support OS {0}', grains['os'])
+
+    return {'SSDs': ssds}
+
+
+def _freebsd_ssds():
+    '''
+    Return a list of disk devices that ATA identify as SSDs (non-rotating)
+    '''
+    SSD_TOKEN = 'non-rotating'
     ssd_devices = []
+
+    devices = __salt__['cmd.run']('sysctl kern.disks', python_shell=False)
+    for device in devices.split(' '):
+        identify = __salt__['cmd.run']('camcontrol identify {0}'.format(
+                                       device), python_shell=False)
+        if SSD_TOKEN in identify:
+            log.trace('Device {0} reports itself as an SSD'.format(device))
+            ssd_devices.append(device)
+        else:
+            log.trace('Device {0} does not report itself as an SSD'.format(
+                      device))
+
+    return ssd_devices
+
+
+def _linux_ssds():
+    '''
+    Return list of disk devices that are SSD (non-rotational)
+    '''
+    ssd_devices = []
+
     for entry in glob.glob('/sys/block/*/queue/rotational'):
         with salt.utils.fopen(entry) as entry_fp:
             device = entry.split('/')[3]
@@ -25,7 +63,9 @@ def ssds():
                 ssd_devices.append(device)
                 log.trace('Device {0} reports itself as an SSD'.format(device))
             elif flag == '1':
-                log.trace('Device {0} does not report itself as an SSD'.format(device))
+                log.trace('Device {0} does not report itself as an SSD'
+                          .format(device))
             else:
-                log.trace('Unable to identify device {0} as an SSD or not. It does not report 0 or 1'.format(device))
-    return {'SSDs': ssd_devices}
+                log.trace('Unable to identify device {0} as an SSD or not.'
+                          ' It does not report 0 or 1'.format(device))
+    return ssd_devices
